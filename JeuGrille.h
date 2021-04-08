a16e6b7296e833fb8aeb9686eb2daa7579a99d34
ReySebastien <74655004+ReySebastien@users.noreply.github.com>
2021-04-08 15:16:00 +0200

    Update JeuGrille.h

--- JeuGrille.h
+++ JeuGrille.h
@@ -15,10 +15,10 @@ class JeuGrille{
         JeuGrille(){}
         virtual void afficheGrille(){}
         virtual void ajouteSymbole(int x, int y){}
-        virtual bool testeVictoireVerticale(){}
-        virtual bool testeVictoireHorizontale(){}
-        virtual bool testeVictoireDiagonale(){}
-        virtual bool testeJeuNul(){}
+        virtual bool testeVictoireVerticale(){return false;}
+        virtual bool testeVictoireHorizontale(){return false;}
+        virtual bool testeVictoireDiagonale(){return false;}
+        virtual bool testeJeuNul(){return false;}
 
         virtual void finTour(){}
 
