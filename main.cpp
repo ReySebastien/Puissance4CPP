945e5f5d0329262e4e6e206a668e6b5183dd367e
ReySebastien <74655004+ReySebastien@users.noreply.github.com>
2021-04-08 15:10:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,23 +9,24 @@
 int main(){
 
     bool partiegagnee = false;
-    JeuGrille jeu;
+    JeuGrille * jeu;
 
     std:: cout << "TicTacToe (0) ou Puissance4 (1) ?";
     int choix;
     std::cin >> choix;
-    if(choix == 0){ jeu = Tictactoe();} else {jeu = Puissance4();}
+    if(choix == 0){ jeu = new Tictactoe();} else {jeu = new Puissance4();}
 
     int numeroCase;
 
 
-    while(!partiegagnee && !jeu.testeJeuNul()){
+    while(!partiegagnee && !jeu->testeJeuNul()){
         
-        jeu.afficheGrille();
+        jeu->afficheGrille();
         std::cin >> numeroCase;
-        jeu.ajouteSymbole(numeroCase%3, numeroCase/3);
-        partiegagnee = jeu.testeVictoireDiagonale() || jeu.testeVictoireHorizontale() || jeu.testeVictoireVerticale();
-        jeu.finTour();
+        if (choix ==0) {jeu->ajouteSymbole(numeroCase%3, numeroCase/3);}
+        else {jeu->ajouteSymbole(numeroCase,0);}
+        partiegagnee = jeu->testeVictoireDiagonale() || jeu->testeVictoireHorizontale() || jeu->testeVictoireVerticale();
+        jeu->finTour();
     }
 
     
